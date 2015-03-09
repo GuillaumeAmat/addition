5c4fa927f98a7573748d202333cf2a7493e54238
barok1383 <replaybarok@altern.org>
2015-03-10 00:36:32 +0100

    correction

--- www/js/require.js
+++ www/js/require.js
@@ -4,21 +4,21 @@ requirejs.config({
 	
 	baseUrl: 'js',
 	paths: {
-		'bower_components': '../bower_components/',
-		'templates': '../templates/',
-		'backbone': '../bower_components/backbone',
-		'backbone.radio': '../bower_components/backbone.radio',
-		'backbone.babysitter': '../bower_components/backbone.babysitter',
-		'jquery': '../bower_components/jquery',
-		'marionette': '../bower_components/marionette',
-		'underscore': '../bower_components/underscore',
-		'webL10n': '../bower_components/webL10n'
-		
+        'bower_components': '../bower_components/',
+        'templates': '../templates/',
+        'backbone': '../bower_components/backbone',
+        'backbone.radio': '../bower_components/backbone.radio',
+        'backbone.babysitter': '../bower_components/backbone.babysitter',
+        'jquery': '../bower_components/jquery',
+        'marionette': '../bower_components/marionette',
+        'underscore': '../bower_components/underscore',
+        'webL10n': '../bower_components/webL10n'
+
 	},
-	
+            
 	deps: [
 		'jquery',
-		'backbone,
+		'backbone',
 		'marionette',
 		'webL10n'
 		
