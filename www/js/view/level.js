c3154e7ba69a45edaa9061d6cfade2684f78ed62
barok1383 <replaybarok@altern.org>
2015-03-11 03:17:00 +0100

    Bouton niveau

--- www/js/view/level.js
+++ www/js/view/level.js
@@ -6,10 +6,25 @@ define(['app', 'templates', 'jquery', 'backbone'], function (app, templates, $, 
 	return Marionette.LayoutView.extend({
 
 		template: JST['level.html'],
+
+        template_button: JST['button_level.html'],
         
         templateHelpers: function() { 
             
             return { 'operator': this.options.operator };
+            
+            
+        },
+
+        
+        onRender: function() { 
+
+            for (var i = 1; i <= 5; i++)
+            {
+                var html = this.template_button({"button_level":"Niveau "+i});
+
+                $("#button_level_"+i, this.$el).html(html);
+            }
             
             
         },
